62c4dea909f2a493e3cef7942fa8f9e9e9413197
AirmanTwelve <twelve.email@protonmail.com>
2026-01-24 10:04:20 -0800

    Edit conf.inc.php File

--- htdocs/includes/conf.inc.php
+++ htdocs/includes/conf.inc.php
@@ -1,7 +1,7 @@
 <?php
-    $db_ip = "*.com";
-    $db_name = "db_airman_record";
-    $db_login = "username";
-    $db_psswd = "password";
-    $site_domain = "*.com";
+    $db_ip = "*.com";               # Change Before Use: (Database Location: IP Address or Hostname)
+    $db_name = "db_airman_record";  # Change Before Use: (Database Name)
+    $db_login = "username";         # Change Before Use: (Database Login Username)
+    $db_psswd = "password";         # Change Before Use: (Database Login Password)
+    $site_domain = "*.com";         # Change Before Use: (Website / App Address / Domain Name)
 ?>
