49d3f1c7267696e9254fadad07834eabcf511df4
AirmanTwelve <twelve.email@protonmail.com>
2026-01-30 02:17:30 -0700

    Edit app.php File: Add HTML Tags

--- htdocs/app/app.php
+++ htdocs/app/app.php
@@ -1,5 +1,25 @@
-<?php
-    include "../includes/conf.inc.php";
-    include "../includes/connect.inc.php";
-    include "../includes/functions.inc.php";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="language" content="en">
+  <meta name="author" content="AirmanTwelve">
+  <meta name="robots" content="index, follow, archive">
+  <meta name="application-name" content="Airman Record">
+  <meta name="keywords" content="Military, Flight, Log, Tracker, Training, Aviation, Record">
+  <meta name="description" content="Airman Record is a Web Based Flight Training Tracker. Airman Record is for Recording Flight Hours and Other Aviation Related Training.">
+
+  <link rel="stylesheet" href="/css/styles.css" />
+  <link rel="shortcut icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>✈️</text></svg>">
+  <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>✈️</text></svg>">
+  
+  <title>Airman Record</title>
+</head>
+
+<body id="body">
+	<?php
+	    include "../includes/conf.inc.php";
+	    include "../includes/connect.inc.php";
+	    include "../includes/functions.inc.php";
+	?>
+</body>
